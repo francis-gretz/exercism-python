0ae711eaeabcdb5cc77e45d3b27356abc0e5bdd1
Francis Gretz <francis.gretz@gmail.com>
2025-06-27 10:41:24 +0000

    Add card games

---
+++ card_games.py
@@ -0,0 +1,94 @@
+"""Functions for tracking poker hands and assorted card tasks.
+
+Python list documentation: https://docs.python.org/3/tutorial/datastructures.html
+"""
+
+
+def get_rounds(number):
+    """Create a list containing the current and next two round numbers.
+
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
+    """
+
+    return list(range(number, number + 3))
+
+
+def concatenate_rounds(rounds_1, rounds_2):
+    """Concatenate two lists of round numbers.
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    """
+
+    return rounds_1 + rounds_2
+
+
+def list_contains_round(rounds, number):
+    """Check if the list of rounds contains the specified number.
+
+    :param rounds: list - rounds played.
+    :param number: int - round number.
+    :return: bool - was the round played?
+    """
+
+    return number in rounds
+
+
+def card_average(hand):
+    """Calculate and returns the average card value from the list.
+
+    :param hand: list - cards in hand.
+    :return: float - average value of the cards in the hand.
+    """
+    return sum(hand) / len(hand)
+
+
+def approx_average_is_average(hand):
+    """Return if the (average of first and last card values) OR ('middle' card) == calculated average.
+
+    :param hand: list - cards in hand.
+    :return: bool - does one of the approximate averages equal the `true average`?
+    """
+
+    calculate_average = card_average(hand)
+
+    # Get the first and last elements average
+    first_and_last_average = card_average([hand[0], hand[-1]])
+
+    # Get the middle card
+    middle_index = get_middle_index(hand)
+    middle_card = hand[middle_index]
+
+    # Check if either the middle card or the average of the first and last cards equals the calculated average
+    return calculate_average in (middle_card, first_and_last_average)
+
+
+def get_middle_index(hand):
+    return len(hand) // 2
+
+
+def average_even_is_average_odd(hand):
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
+
+    :param hand: list - cards in hand.
+    :return: bool - are even and odd averages equal?
+    """
+    evens = hand[::2]  # Elements at even indices
+    odds = hand[1::2]  # Elements at odd indices
+
+    return card_average(evens) == card_average(odds)
+
+
+def maybe_double_last(hand):
+    """Multiply a Jack card value in the last index position by 2.
+
+    :param hand: list - cards in hand.
+    :return: list - hand with Jacks (if present) value doubled.
+    """
+
+    if hand[-1] == 11:
+        return hand[:-1] + [22]
+
+    return hand
